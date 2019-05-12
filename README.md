4ccf324af0ed3945011e6830ce6531949775179a
Zoe <pinasramad12@gmail.com>
2019-05-12 22:50:19 +0300

    README FILE ADDED

---
+++ README.md
@@ -0,0 +1,3 @@
+Log Util Library
+
+Welcome to Log Util Documentation
--- logutil/src/main/java/logutil/scratchlabs/android/logutil/LogDebug.java
+++ logutil/src/main/java/logutil/scratchlabs/android/logutil/LogDebug.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class LogDebug {
 
-    private static final String TAG = "SUPER_WESOME_APP";
+    private static final String TAG = "SUPER_AWESOME_APP";
 
     public static void d(String message)
     {
